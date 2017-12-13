fc594b83bc7be973ef8ab272a911179f0d219519
Daniel Lui <dan_deltaice@yahoo.com>
2017-12-12 19:40:29 -0800

    Create out.m
    output files

---
+++ out/out.m
@@ -0,0 +1,12 @@
+   12-Dec-2017 19:23:12
+
+    'Patent Lawyer'      [4181999.26]
+    'Missionary'         [ 498074.80]
+    'Physics Teacher'    [3120445.08]
+    'Developer'          [1929923.32]
+
+    'Patent Lawyer'      [4847915.93]    '±'    [1390063.70]
+    'Missionary'         [ 590018.68]    '±'    [ 193041.05]
+    'Physics Teacher'    [3061984.75]    '±'    [ 132898.86]
+    'Software Dev'       [2281516.72]    '±'    [ 736770.93]
+
