3035e28cd989b9e653d3efacfd11f02f45ef2e01
Daniel Lui <dan_deltaice@yahoo.com>
2017-12-12 19:39:02 -0800

    Update ME175A_Project.m

--- ME175A_Project.m
+++ ME175A_Project.m
@@ -31,12 +31,6 @@ i = d*((x+y)-1);
 
 new_marr = marr.*(1+i);
 
-% new_marr = marr.*dev_fac;
-
-
-% hist(new_marr)
-
-
 % Each of the 4 scenarios 
 pv1=0; pv2=0; pv3=0; pv4=0; % Initialize all present values as 0
 
@@ -44,6 +38,19 @@ salary1 = 150000; tuition1 = 45338;
 salary2 = 31600; 
 salary3e = 68268; salary3t = 50188; tuition3 = 17178; 
 salary4 = 120000; 
+
+% Present Value Calbulation without MARR variation
+
+% Patent Lawyer
+pv1a = f1('P/F',0.06,0,4)*(salary1*f1('P/A1',0.06,0.03,40)) - tuition1*f1('P/F',0.06,0,1)*f1('P/A',0.06,0,3);
+% Missionary
+pv2a = salary2*f1('P/A',0.06,0,50);
+% Physics Teacher
+pv3a = salary3e*f1('P/A1',0.06,0.03,20) + salary3t*f1('P/F',0.06,0,20)*f1('P/A',0.06,0,28) - tuition3*f1('P/A',0.06,0,2);        
+% Nanodegree>Software Developer
+pv4a = -2000 + f1('P/F',0.06,0,1)*(salary4*f1('P/A',0.06,0,47) - 0.15*salary4*f1('P/A',0.06,0,3));
+
+% Simulation with MARR +/- deviation (50%)
 
 for b=1:n
     % Patent Lawyer
@@ -57,11 +64,20 @@ for b=1:n
 end
 
 % output cell
-out = {'Patent Lawyer' , mean(pv1) , '±', 1.96*std(pv1); 'Missionary', mean(pv2) , '±', 1.96*std(pv2); 'Physics Teacher' mean(pv3) , '±', 1.96*std(pv3); 'Software Dev' mean(pv4) , '±', 1.96*std(pv4)};
 
-disp(out)
+out1={'Patent Lawyer' , pv1a ; 'Missionary', pv2a ; 'Physics Teacher' , pv3a; 'Developer', pv4a};
 
 
+out2 = {'Patent Lawyer' , mean(pv1) , 'Â±', 1.96*std(pv1); 'Missionary', mean(pv2) , 'Â±', 1.96*std(pv2); 'Physics Teacher' mean(pv3) , 'Â±', 1.96*std(pv3); 'Software Dev' mean(pv4) , 'Â±', 1.96*std(pv4)};
+
+
+diary out.m % output to a text file
+
+disp(datetime('now')) % timestamp
+disp(out1) % raw calculation cell
+disp(out2) % sensitivity analysis cell
+
+diary off
 
 
 
